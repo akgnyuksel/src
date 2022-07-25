22e7297188a1ffcf82697ea73520c2ea1acd0672
akgnyuksel <yuksel.anka@gmail.com>
2022-07-25 08:25:08 +0300

    Garbage Collection

--- day34_GarbageCollection/Constructor_vs_StaticBlock.java
+++ day34_GarbageCollection/Constructor_vs_StaticBlock.java
@@ -1,2 +1,19 @@
-package day34_GarbageCollection;public class Constructor_vs_StaticBlock {
+package day34_GarbageCollection;
+
+public class Constructor_vs_StaticBlock {
+
+    static {
+        System.out.println("Static Block");
+    }
+    public Constructor_vs_StaticBlock(){
+        System.out.println("Constructor");
+    }
+
+    public static void main(String[] args) {
+        new Constructor_vs_StaticBlock();
+        new Constructor_vs_StaticBlock();
+        new Constructor_vs_StaticBlock();
+        new Constructor_vs_StaticBlock();
+        new Constructor_vs_StaticBlock();
+    }
 }
