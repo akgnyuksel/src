5d41dadad27f00244ee1f6a1bee6dbf55c807bb2
akgnyuksel <yuksel.anka@gmail.com>
2022-08-01 11:30:05 +0300

    Last Updated

---
+++ day39_EncapsulationAndInheritance/shapeTask/Circle.java
@@ -0,0 +1,42 @@
+package day39_EncapsulationAndInheritance.shapeTask;
+
+public class Circle extends Shape{
+    private double radius;
+    public static double pi=3.14;
+
+    public Circle(String name, double radius) {
+        super(name);
+        setRadius(radius);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if(radius<=0){
+            System.err.println("Invalid Radius "+radius);
+            System.exit(1);
+        }
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return radius*radius*pi;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2*pi*radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", area=" + area() +
+                ", perimeter=" + perimeter() +
+                '}';
+    }
+}
