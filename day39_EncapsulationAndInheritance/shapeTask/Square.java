2ca3ce2a36e9a30073b82aad5d6d4e90751b94ca
akgnyuksel <yuksel.anka@gmail.com>
2022-08-01 10:59:58 +0300

    day38:Inheritance

---
+++ day39_EncapsulationAndInheritance/shapeTask/Square.java
@@ -0,0 +1,41 @@
+package day39_EncapsulationAndInheritance.shapeTask;
+
+public class Square extends Shape{
+    private double side;
+
+    public Square(String name, double side) {
+        super(name);
+       setSide(side);
+    }
+
+    public Square(String name) {
+        super(name);
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if(side<=0){
+            System.err.println("Invalid side "+side);
+            System.exit(1);
+        }
+        this.side = side;
+    }
+
+    @Override
+    public double area() {
+        return side*side;
+    }
+
+    @Override
+    public double perimeter() {
+        return side*4;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
